d9ac00f499e923daf98d52502689aaf871a5e457
Santi Menendez <santiagomenendez@hotmail.com>
2023-01-05 09:17:12 +0100

    Uso de componentes de terceros

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 import router from "@/router";
+import Chart from "chart.js";
+import Chartkick from "vue-chartkick";
+import { VueSpinners } from "@saeris/vue-spinners";
 import { dollarFilter, percentFilter } from "@/filter";
+
+Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart));
 
 Vue.filter("dollar", dollarFilter);
 Vue.filter("percent", percentFilter);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
     <px-assets-table v-bind:assets="assets" />
   </div>
 </template>
@@ -12,11 +13,16 @@ export default {
   components: { PxAssetsTable },
   data() {
     return {
+      isLoading: false,
       assets: [],
     };
   },
   created() {
-    api.getAssets().then((assets) => (this.assets = assets));
+    this.isLoading = true;
+    api
+      .getAssets()
+      .then((assets) => (this.assets = assets))
+      .finally(() => (this.isLoading = false));
   },
 };
 </script>
